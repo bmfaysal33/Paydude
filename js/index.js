011829eb6be8710a19b7be8da4f7d100ad88ede2
BM Faysal <bmfaysal33@gmail.com>
2025-09-06 09:06:36 +0600

    home page nav done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('Connected code...')
+
+
+document.getElementById('login').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const numInput = document.getElementById('number-btn').value;
+    const pinInput = document.getElementById('pin-btn').value;
+
+    const converNum = parseInt(numInput);
+    const converPin = parseInt(pinInput);
+
+    // console.log(typeof converNum, typeof converPin);
+    
+  
+        if ( converNum === 1234 && converPin === 1234) {
+            // alert('Password Correct ✅✅✅')
+            window.location.href= 'home.html'
+
+        }
+    
+
+    console.log('Button clicked')
+
+})
