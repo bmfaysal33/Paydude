22f226d76d0c5169f19c95edeb8c6d97574e5233
BM Faysal <bmfaysal33@gmail.com>
2025-09-12 18:22:58 +0600

    cashout form done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,23 @@
-console.log('Home JS file ')
+
+
+document.getElementById('money-add-btn').addEventListener('click', function(e){
+
+e.preventDefault();
+
+ const addInputPin = document.getElementById('add-input-pin');
+ const amountInput = document.getElementById('add-amount-input');
+
+ const pinNum = addInputPin.value;
+const inputAmount = amountInput.value;
+
+if(pinNum === '1234'){
+     const currentBalance = document.getElementById('available-balance');
+     const balanceNow = parseFloat(currentBalance) + parseFloat(inputAmount);
+     currentBalance.innerText = balanceNow;
+
+ }
+ else {
+    alert('Invalid pin')
+ }
+
+})
